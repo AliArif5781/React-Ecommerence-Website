fd62e4b3f504e41b38ac101f237064258a24ddf3
AliArif5781 <aliairf92@gmail.com>
2024-09-20 13:06:58 +0500

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import img from "/logo_black.svg";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import img1 from "/logo_white.svg";
